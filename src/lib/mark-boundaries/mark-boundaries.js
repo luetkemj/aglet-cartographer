dd288f83c1dfc020298ba03898937b49b57d8ea0
Mark Luetke <luetkemj@gmail.com>
2017-12-26 20:48:05 -0600

    mark boundaries function
    this one makes sense as that is what it does. the others just give boundary ids

--- src/lib/mark-boundaries/mark-boundaries.js
+++ src/lib/mark-boundaries/mark-boundaries.js
@@ -1,35 +1,16 @@
 // @flow
-
-import { hexNeighbor, idToHex, markBoundary } from '../../index.prod';
+import { cloneDeep, each } from 'lodash';
 
 // from a startHex get all boundary hexes for a rectangle with n columns and rows
 export default function markBoundaries(
-  hex: { x: number, y: number, z: number },
-  columns: number,
-  rows: number,
-  hexes: Array<Object>,
-): Array<Object> {
-  let idMapBoundaries = [];
-  let startHex = hex;
+  hexes: Object,
+  idMapBoundaries: Array<String>,
+): Object {
+  const newHexes = cloneDeep(hexes);
 
-  // north boundary
-  idMapBoundaries =
-    idMapBoundaries.concat(markBoundary(startHex, columns, false, false, hexes));
+  each(idMapBoundaries, (id) => {
+    newHexes[id].isBoundary = true;
+  });
 
-  // east boundary
-  startHex = hexNeighbor(idToHex(idMapBoundaries[idMapBoundaries.length - 1]), 2);
-  idMapBoundaries =
-    idMapBoundaries.concat(markBoundary(startHex, rows - 1, true, false, hexes));
-
-  // west boundary
-  startHex = hexNeighbor(hex, 2);
-  idMapBoundaries =
-    idMapBoundaries.concat(markBoundary(startHex, rows - 1, true, false, hexes));
-
-  // southern boundary
-  startHex = hexNeighbor(idToHex(idMapBoundaries[idMapBoundaries.length - 1]), 1);
-  idMapBoundaries =
-    idMapBoundaries.concat(markBoundary(startHex, columns - 2, false, true, hexes));
-
-  return idMapBoundaries;
+  return newHexes;
 }
